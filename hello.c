be04c4f5350ee540e1d4c8786beaeb178d222da5
Yongmin Yan <yongminyan@gmail.com>
2014-11-07 16:54:24 -0800

    Update hello.c
    again

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-    printf("Hello World Again!\n");
+    printf("Hello World Again Again!\n");
     printf("Hello World Again!\n"); // take both and commit
     printf("Test Git Fetch!\n");
     return 0;
